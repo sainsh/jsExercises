39ae4da3669a49d674948c4b46c99b4aca01d6b4
sainsh <kfsainsh@gmail.com>
2019-09-25 09:58:06 +0200

    added class example

---
+++ classExcersise.js
@@ -0,0 +1,10 @@
+
+let creativeObj = {};
+creativeObj.navn = "Karsten";
+creativeObj.alder = 43;
+creativeObj.boern = ["Lise", "Karl", "Daniel", "soeren"];
+creativeObj.sigHej = () => {for(let element of creativeObj.boern){console.log("hej " + element);}}
+
+
+console.log(creativeObj);
+creativeObj.sigHej();
